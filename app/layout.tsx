c9b349eadc9bec34e5194ce119e8f2cdf35d0025
Al-Ameen <belloalameenn@outlook.com>
2025-04-25 12:43:01 +0100

    project start

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const jetBrainsMono = JetBrains_Mono({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "bMeen",
+  description: "Bello Al-Ameen: FrontEnd Developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${jetBrainsMono.className} antialiased bg-background text-text`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
