5d6438b6295a2b6fb883e6dc389593ae96197643
bMeen <belloalameenn@outlook.com>
2026-03-30 16:48:46 +0100

    project descriptions

--- app/_components/projects/project.ts
+++ app/_components/projects/project.ts
@@ -19,23 +19,23 @@ export const projects: Project[] = [
       "A modern, responsive landing page built to showcase Venstphere, a platform that supports startups by providing funding and strategic guidance, highlighting the company’s mission to empower visionary entrepreneurs",
     skills: ["Nextjs", "Tailwindcss", "Framer Motion", "Typescript"],
     url: "https://ventsphere-azure.vercel.app/",
-    image: venstphere,
+    image: venstphere
   },
   {
     title: "ClassroomHq",
     description:
-      "A modern, sleek classroom management dashboard built for teachers, It helps manage student records, track grades and attendance, and visualize class performance — all in one place.",
+      "A modern, sleek classroom management dashboard built for teachers, It helps manage student records, track grades and attendance, and visualize class performance",
     skills: ["React", "Tailwindcss", "Framer Motion", "Typescript"],
     url: "https://classroomhq.vercel.app/",
-    image: classroomhq,
+    image: classroomhq
   },
   {
     title: "RideXpress",
     description:
-      "A web-based transport booking MVP designed to simplify how users browse available trips, reserve seats, and complete payments. The goal is to create a lightweight but scalable app for transport operators while practicing real-world product development.",
+      "A seamless platform for booking intercity trips and renting vehicles in minutes.",
     skills: ["Nextjs", "Tailwindcss", "Supabase", "Typescript", "Shadcn UI"],
     url: "https://ridexpress-dun.vercel.app/",
     image: rideXpress,
-    wip: true,
-  },
+    wip: true
+  }
 ];
